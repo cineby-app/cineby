a38fa18ba98612980b08a802cf5fe753b4fb62ea
Paul <breadpaul@proton.me>
2026-09-20 10:26:23 -0700

    fix robots file

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,6 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/api/', '/_next/', '/tmdb/',],
     },
     sitemap: 'https://cineby.vip/sitemap.xml',
   };
